b9487df6bc07be977af6148107e1d62f8b5e3c5f
scott <jw62czgn8n@privaterelay.appleid.com>
2026-08-09 21:57:54 +0800

    Add files via upload

---
+++ day 1.py
@@ -0,0 +1,54 @@
+print(3 + 4)
+print(3 - 4)
+print(3 * 4)
+print(3 % 4)
+print(3 / 4)
+print(3 ** 4)
+print(3 // 4)
+
+'Scott' # Name
+'San Miguel' # Family Name
+'Philippines' # Country
+'I am enjoying 30 Days of Python'
+
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4 - 4j))
+print(type(['Asabeneh', 'Python', 'Finland']))
+print(type('Scott'))
+print(type('San Miguel'))
+print(type('Philippines'))
+
+# Give example of the following:
+    # Number: integer, Float, Complex
+    # String
+    # Boolean
+    # List
+    # Tuple
+    # Set
+    # Dictionary
+
+[1, -3.2, 3 + 4j] # Number: integer, Float, Complex
+'Scott' # String
+True # Boolean
+[1, 2, 3] # List
+(1, 2, 3) # Tuple
+{1, 2, 3} # Set
+{'name': 'Scott', 'age': 27} # Dictionary
+
+
+# Find an Euclidean distance between (2, 3) and (10, 8)
+
+# dimension 1: (2, 3)
+p1 = 2
+q1 = 3
+
+# dimension 2
+p2 = 10
+q2 = 8
+
+distance = ((p2 - p1) ** 2 + (q2 - q1) ** 2)**0.5
+
+print('The Euclidean distance between (2, 3) and (10, 8) is:')
+print(distance)
